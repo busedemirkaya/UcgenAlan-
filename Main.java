46f171a75a30b48294190c4b4e1923206c109b78
Buse Demirkaya <112807956+busedemirkaya@users.noreply.github.com>
2022-10-07 13:44:04 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Üçgenin Birinci Kenar Uzunluğunu Giriniz: ");
+        a = input.nextInt();
+        System.out.print("Üçgenin İkinci Kenar Uzunluğunu Giriniz: ");
+        b = input.nextInt();
+        System.out.print("Üçgenin Üçüncü Kenar Uzunluğunu Giriniz: ");
+        c = input.nextInt();
+        int u = (a+b+c) / 2;
+        int alan = u*(u-a)*(u-b)*(u-c);
+        System.out.println("Üçgenin Alanı: "+ alan);
+    }
+}
